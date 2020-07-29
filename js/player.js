917a0cde01127d515fcf5dd73cec87c5673ca583
chamson <chamson1995@qq.com>
2020-07-29 20:29:03 +0800

    Initial commit

---
+++ js/player.js
@@ -0,0 +1,16 @@
+let myPlayer = {
+    watch: {
+        current:function(value){
+            
+        }
+    },
+    data() {
+        return {
+            current:90
+        }
+    },
+    template:`
+
+    `
+        
+}
